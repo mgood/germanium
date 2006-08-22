0d7882aae16609922770272fe65fe4c7292f4426
Matt Good <matt@matt-good.net>
2006-08-22 15:56:45 -0700

    add missing import to gconf_util
    darcs-hash:20060822225645-25259-3cdd977c4517501d3d9a42e18a2c16a284c33b65.gz

--- emusic/gconf_util.py
+++ emusic/gconf_util.py
@@ -16,9 +16,10 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
+import os.path
+
 import gconf
 import gtk
-
 
 class gconf_property(object):
     def __init__(self, key, type=gconf.VALUE_STRING):
