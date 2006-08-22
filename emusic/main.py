786a6fdbe4ca242b2641238251380ef825090b00
Matt Good <matt@matt-good.net>
2006-08-21 18:17:32 -0700

    package reorg
    darcs-hash:20060822011732-25259-8ce1d1d54f3b02359101fa26976035b771f3a4bf.gz

--- emusic/main.py
+++ emusic/main.py
@@ -18,16 +18,12 @@ from twisted.internet import reactor
 
 import errno
 import os.path
-import time
 from threading import Thread
+from urlparse import urlparse
 from Queue import Queue, Empty
 
-from urlgrabber.grabber import URLGrabber, URLGrabError
-from urlgrabber.progress import BaseMeter
-
-from twisted.web import client
-
-from emusic.decrypt import get_tracks
+from emusic.emp import get_tracks
+from emusic.progress import ProgressDownloader, format_time
 
 TITLE_COLUMN = 0
 PROGRESS_COLUMN = 1
@@ -41,7 +37,6 @@ icons = {
     'completed': gtk.STOCK_APPLY,
     'error': gtk.STOCK_DIALOG_ERROR,
 }
-
 
 class EmusicDownloader(object):
 
@@ -184,9 +179,9 @@ class EmusicDownloader(object):
             if e.errno != errno.EEXIST:
                 raise
 
-        scheme,host,port,path = client._parse(url)
+        scheme,host,path,_,_,_ = urlparse(url)
         factory = ProgressDownloader(url, filename, started, progress)
-        reactor.connectTCP(host, port, factory)
+        reactor.connectTCP(host, 80, factory)
         factory.deferred.addCallback(complete).addErrback(error)
         track.start(factory)
 
@@ -232,165 +227,6 @@ class Track(object):
         self.status = 'error'
 
 
-class ProgressDownloader(client.HTTPDownloader):
-    def __init__(self, url, file, started_callback, progress_callback,
-                 granularity=0.3):
-        client.HTTPDownloader.__init__(self, url, file)
-        self.started_callback = started_callback
-        self.progress_callback = progress_callback
-        self.granularity = granularity
-        self.next_update = None
-        self.re = RateEstimator()
-        self._protocol = None
-
-    def buildProtocol(self, addr):
-        self._protocol = client.HTTPDownloader.buildProtocol(self, addr)
-        return self._protocol
-
-    def abort(self):
-        self._protocol.transport.loseConnection()
-
-    def gotHeaders(self, headers):
-        if self.status == '200':
-            self.total_length = int(headers.get('content-length', [0])[0])
-            self.re.start(self.total_length)
-            self.current_length = 0.0
-        return client.HTTPDownloader.gotHeaders(self, headers)
-
-    def pageStart(self, data):
-        self.started_callback()
-        return client.HTTPDownloader.pageStart(self, data)
-
-    def pagePart(self, data):
-        if self.status == '200':
-            self.current_length += len(data)
-            if self.total_length:
-                now = time.time()
-                self.re.update(self.current_length, now)
-                if not self.next_update or now >= self.next_update:
-                    self.next_update = now + self.granularity
-                    #self.progress_callback(self.current_length / self.total_length)
-                    self.progress_callback(self.re)
-        return client.HTTPDownloader.pagePart(self, data)
-
-
-class RateEstimator:
-    def __init__(self, timescale=5.0):
-        self.timescale = timescale
-
-    def start(self, total=None, now=None):
-        if now is None: now = time.time()
-        self.total = total
-        self.start_time = now
-        self.last_update_time = now
-        self.last_amount_read = 0
-        self.ave_rate = None
-        
-    def update(self, amount_read, now=None):
-        if now is None: now = time.time()
-        if amount_read == 0:
-            # if we just started this file, all bets are off
-            self.last_update_time = now
-            self.last_amount_read = 0
-            self.ave_rate = None
-            return
-
-        #print 'times', now, self.last_update_time
-        time_diff = now         - self.last_update_time
-        read_diff = amount_read - self.last_amount_read
-        self.last_update_time = now
-        self.last_amount_read = amount_read
-        self.ave_rate = self._temporal_rolling_ave(\
-            time_diff, read_diff, self.ave_rate, self.timescale)
-        #print 'results', time_diff, read_diff, self.ave_rate
-        
-    #####################################################################
-    # result methods
-    def average_rate(self):
-        "get the average transfer rate (in bytes/second)"
-        return self.ave_rate
-
-    def elapsed_time(self):
-        "the time between the start of the transfer and the most recent update"
-        return self.last_update_time - self.start_time
-
-    def remaining_time(self):
-        "estimated time remaining"
-        if not self.ave_rate or not self.total: return None
-        return (self.total - self.last_amount_read) / self.ave_rate
-
-    def fraction_read(self):
-        """the fraction of the data that has been read
-        (can be None for unknown transfer size)"""
-        if self.total is None: return None
-        elif self.total == 0: return 1.0
-        else: return float(self.last_amount_read)/self.total
-
-    #########################################################################
-    # support methods
-    def _temporal_rolling_ave(self, time_diff, read_diff, last_ave, timescale):
-        """a temporal rolling average performs smooth averaging even when
-        updates come at irregular intervals.  This is performed by scaling
-        the "epsilon" according to the time since the last update.
-        Specifically, epsilon = time_diff / timescale
-
-        As a general rule, the average will take on a completely new value
-        after 'timescale' seconds."""
-        epsilon = time_diff / timescale
-        if epsilon > 1: epsilon = 1.0
-        return self._rolling_ave(time_diff, read_diff, last_ave, epsilon)
-    
-    def _rolling_ave(self, time_diff, read_diff, last_ave, epsilon):
-        """perform a "rolling average" iteration
-        a rolling average "folds" new data into an existing average with
-        some weight, epsilon.  epsilon must be between 0.0 and 1.0 (inclusive)
-        a value of 0.0 means only the old value (initial value) counts,
-        and a value of 1.0 means only the newest value is considered."""
-        
-        try:
-            recent_rate = read_diff / time_diff
-        except ZeroDivisionError:
-            recent_rate = None
-        if last_ave is None: return recent_rate
-        elif recent_rate is None: return last_ave
-
-        # at this point, both last_ave and recent_rate are numbers
-        return epsilon * recent_rate  +  (1 - epsilon) * last_ave
-
-    def _round_remaining_time(self, rt, start_time=15.0):
-        """round the remaining time, depending on its size
-        If rt is between n*start_time and (n+1)*start_time round downward
-        to the nearest multiple of n (for any counting number n).
-        If rt < start_time, round down to the nearest 1.
-        For example (for start_time = 15.0):
-         2.7  -> 2.0
-         25.2 -> 25.0
-         26.4 -> 26.0
-         35.3 -> 34.0
-         63.6 -> 60.0
-        """
-
-        if rt < 0: return 0.0
-        shift = int(math.log(rt/start_time)/math.log(2))
-        rt = int(rt)
-        if shift <= 0: return rt
-        return float(int(rt) >> shift << shift)
-
-def format_time(seconds, use_hours=0):
-    if seconds is None or seconds < 0:
-        if use_hours: return '--:--:--'
-        else:         return '--:--'
-    else:
-        seconds = int(seconds)
-        minutes = seconds / 60
-        seconds = seconds % 60
-        if use_hours:
-            hours = minutes / 60
-            minutes = minutes % 60
-            return '%02i:%02i:%02i' % (hours, minutes, seconds)
-        else:
-            return '%02i:%02i' % (minutes, seconds)
- 
 if __name__ == '__main__':
     try:
         emusic = EmusicDownloader(sys.argv[1:])
